6941fa503a84445cefb19e5f5406c1659f1edfbe
nvq29Apr <nvq29apr@gmail.com>
2023-09-29 16:57:47 +0700

    responsive complete

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// image slider
 const productContainers = [...document.querySelectorAll('#feedback .container')];
 const nextBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
@@ -14,3 +15,20 @@ productContainers.forEach((item, i) => {
         item.scrollLeft += containerWidth;
     })
 })
+
+// open/close navbar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if(close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
